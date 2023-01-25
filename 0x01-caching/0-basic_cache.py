3c0186db45fc7bef66abe4fc513325e07e41fd94
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-25 11:30:39 +0300

    adding to and reterive data from cache dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Module 0-basic_cache
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A class used to represent basic cache
+
+    Arguments
+    ---------
+    key: str
+    key name of cache_data dictionary
+    item: str
+        value corresponding to a given key of cache_data dictionary
+    Methods
+    -------
+    put(key, item)
+        Add an item in the cache
+    get(key)
+        Get an item by key
+    """
+    def put(self, key, item):
+        """
+        Add an item in the cache
+
+        Parameters
+        ----------
+        key: str
+          key name of cache_data dictionary
+        item: str
+            value corresponding to a given key of cache_data dictionary
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+
+        Parameters
+        ----------
+        key: str
+            key name of cache_data dictionar
+
+        Returns
+        -------
+        str
+          item corresponding to a given key or
+          none if the key is none or does't exist
+        """
+        if key is None or key not in self.cache_data:
+            return
+        return self.cache_data.get(key)

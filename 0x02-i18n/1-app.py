b70673531e4fd8b046ad9fe89fb17fcf66d78b95
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-31 12:11:56 +0300

    Get locale from request

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 """
-Module 0-app
+Module 1-app
 Set up basic Flask application
 """
 
 from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A class that represent available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+
 
 app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = "en"
+app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/")
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module 2-app
+Set up basic Flask application
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A class that represent available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = "en"
+app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.babel.localeselector
+def get_locale():
+    """select a language translation to use for the request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """Display Hello World message
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

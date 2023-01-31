ca7fa2553ff614bd88eb5c69efe7714c6e31ba7d
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-31 17:27:15 +0300

    minor change

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -63,7 +63,7 @@ def get_locale():
 def index() -> str:
     """Display Hello World message
     """
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":

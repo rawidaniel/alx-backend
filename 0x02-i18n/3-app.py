50d461c896a0c7095971e2b78649ab121406d745
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-31 12:49:38 +0300

    add file

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Module 2-app
+Set up basic Flask application
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A class that represent available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """select a language translation to use for the request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index() -> str:
+    """Display Hello World message
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

7e17c5fb4aad6c2d9e3359cdccbd6db14e02009d
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-31 18:08:38 +0300

    fix bug

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -67,7 +67,7 @@ def get_locale():
 def index() -> str:
     """Display Hello World message
     """
-    return render_template("5-index.html")
+    return render_template("6-index.html")
 
 
 if __name__ == "__main__":
